a2f9d347700e09b316fd28af31ece96b790fa130
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-10-10 13:20:53 +0700

    Refactor the code

--- lib/ui/restaurant_details_page.dart
+++ lib/ui/restaurant_details_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:restaurian/common/styles.dart';
+import 'package:restaurian/data/model/drink.dart';
+import 'package:restaurian/data/model/food.dart';
 import 'package:restaurian/data/model/restaurant.dart';
 
 class RestaurantDetailsPage extends StatelessWidget {
@@ -45,8 +48,179 @@ class RestaurantDetailsPage extends StatelessWidget {
           ];
         },
         body: ListView(
-          children: [],
+          padding: const EdgeInsets.all(
+            16.0,
+          ),
+          children: [
+            _buildRating(
+              restaurant.rating,
+            ),
+            _buildLocation(
+              restaurant.city,
+            ),
+            const Divider(
+              height: 12.0,
+              color: Colors.transparent,
+            ),
+            _buildDescription(
+              restaurant.description,
+            ),
+            const Divider(
+              height: 12.0,
+              color: Colors.transparent,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: _buildAvailableFoods(
+                    context,
+                    restaurant.menus.foods,
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: _buildAvailableDrinks(
+                    context,
+                    restaurant.menus.drinks,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildRating(double rating) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          WidgetSpan(
+            child: Icon(
+              rating < 2.0
+                  ? Icons.star_outline
+                  : rating < 4.0
+                      ? Icons.star_half
+                      : Icons.star,
+              size: 30.0,
+            ),
+          ),
+          const WidgetSpan(
+            child: SizedBox(
+              width: 12.0,
+            ),
+          ),
+          TextSpan(
+            text: rating.toString(),
+            style: myTextTheme.headline4!.copyWith(
+              overflow: TextOverflow.ellipsis,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLocation(String location) {
+    return Text(
+      'This restaurant is located in $location.',
+      maxLines: 2,
+      style: myTextTheme.subtitle1!.copyWith(
+        overflow: TextOverflow.ellipsis,
+        color: Colors.black54,
+      ),
+    );
+  }
+
+  Widget _buildDescription(String description) {
+    return Text(
+      description,
+      textAlign: TextAlign.justify,
+      style: myTextTheme.subtitle2!.copyWith(
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Widget _buildAvailableFoods(BuildContext context, List<Food> foods) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'Available foods.',
+            textAlign: TextAlign.center,
+            style: myTextTheme.subtitle1!.copyWith(
+              color: Colors.black54,
+            ),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.fastfood,
+              size: 50.0,
+            ),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return ListView.builder(
+                    itemCount: foods.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.fastfood),
+                        title: Text(
+                          foods[index].name,
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAvailableDrinks(BuildContext context, List<Drink> drinks) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            'Available drinks.',
+            textAlign: TextAlign.center,
+            style: myTextTheme.subtitle1!.copyWith(
+              color: Colors.black54,
+            ),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.emoji_food_beverage_sharp,
+              size: 50.0,
+            ),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return ListView.builder(
+                    itemCount: drinks.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.emoji_food_beverage_sharp),
+                        title: Text(
+                          drinks[index].name,
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
