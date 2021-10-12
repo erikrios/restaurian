cc6950f0185a3d79f4049d7579f5a848f20dc921
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-10-12 11:38:23 +0700

    Add the consumer

--- lib/ui/restaurant_details_page.dart
+++ lib/ui/restaurant_details_page.dart
@@ -1,95 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurian/common/styles.dart';
 import 'package:restaurian/data/model/drink.dart';
 import 'package:restaurian/data/model/food.dart';
 import 'package:restaurian/data/model/restaurant.dart';
+import 'package:restaurian/data/model/restaurant_detail_response.dart';
+import 'package:restaurian/provider/restaurant_detail_provider.dart';
+import 'package:restaurian/provider/result_state.dart';
 
 class RestaurantDetailsPage extends StatelessWidget {
   static const routeName = "/restaurants/details";
 
-  final Restaurant restaurant;
-
-  const RestaurantDetailsPage({Key? key, required this.restaurant})
-      : super(key: key);
+  const RestaurantDetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (context, isScroller) {
-          return [
-            SliverAppBar(
-              pinned: true,
-              expandedHeight: 200,
-              iconTheme: const IconThemeData(
-                color: Colors.white,
-              ),
-              flexibleSpace: FlexibleSpaceBar(
-                background: Hero(
-                  tag: restaurant.id,
-                  child: Image.network(
-                    restaurant.pictureId,
-                    fit: BoxFit.fitWidth,
+    return Consumer<RestaurantDetailProvider>(
+      builder: (context, provider, _) {
+        ResultState<RestaurantDetailResponse> state = provider.state;
+        switch (state.status) {
+          case Status.loading:
+            return const CircularProgressIndicator();
+          case Status.error:
+            return Center(
+              child: Text(state.message!),
+            );
+          case Status.hasData:
+            {
+              Restaurant restaurant = state.data!.restaurant;
+              return Scaffold(
+                body: NestedScrollView(
+                  headerSliverBuilder: (context, isScroller) {
+                    return [
+                      SliverAppBar(
+                        pinned: true,
+                        expandedHeight: 200,
+                        iconTheme: const IconThemeData(
+                          color: Colors.white,
+                        ),
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: Hero(
+                            tag: restaurant.id,
+                            child: Image.network(
+                              restaurant.mediumPictureUrl,
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                          title: Text(
+                            restaurant.name,
+                            style: myTextTheme.headline6!.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                          titlePadding: const EdgeInsets.only(
+                            left: 48.0,
+                            bottom: 16.0,
+                          ),
+                        ),
+                      ),
+                    ];
+                  },
+                  body: ListView(
+                    padding: const EdgeInsets.all(
+                      16.0,
+                    ),
+                    children: [
+                      _buildRating(
+                        restaurant.rating.toDouble(),
+                      ),
+                      _buildLocation(
+                        restaurant.city,
+                      ),
+                      const Divider(
+                        height: 12.0,
+                        color: Colors.transparent,
+                      ),
+                      _buildDescription(
+                        restaurant.description,
+                      ),
+                      const Divider(
+                        height: 12.0,
+                        color: Colors.transparent,
+                      ),
+                      Row(
+                        children: [
+                          Flexible(
+                            flex: 1,
+                            child: _buildAvailableFoods(
+                              context,
+                              restaurant.menus.foods,
+                            ),
+                          ),
+                          Flexible(
+                            flex: 1,
+                            child: _buildAvailableDrinks(
+                              context,
+                              restaurant.menus.drinks,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                title: Text(
-                  restaurant.name,
-                  style: myTextTheme.headline6!.copyWith(
-                    color: Colors.white,
-                  ),
-                ),
-                titlePadding: const EdgeInsets.only(
-                  left: 48.0,
-                  bottom: 16.0,
-                ),
-              ),
-            ),
-          ];
-        },
-        body: ListView(
-          padding: const EdgeInsets.all(
-            16.0,
-          ),
-          children: [
-            _buildRating(
-              restaurant.rating.toDouble(),
-            ),
-            _buildLocation(
-              restaurant.city,
-            ),
-            const Divider(
-              height: 12.0,
-              color: Colors.transparent,
-            ),
-            _buildDescription(
-              restaurant.description,
-            ),
-            const Divider(
-              height: 12.0,
-              color: Colors.transparent,
-            ),
-            Row(
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: _buildAvailableFoods(
-                    context,
-                    restaurant.menus.foods,
-                  ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: _buildAvailableDrinks(
-                    context,
-                    restaurant.menus.drinks,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+              );
+            }
+        }
+      },
     );
   }
 
