5e7db4060f793a0fac89bd9154680b6415067889
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-10-12 11:14:29 +0700

    Add multi providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurian/common/styles.dart';
+import 'package:restaurian/data/api/api_service.dart';
 import 'package:restaurian/data/model/restaurant.dart';
+import 'package:restaurian/provider/restaurant_detail_provider.dart';
+import 'package:restaurian/provider/restaurants_provider.dart';
 import 'package:restaurian/ui/restaurant_details_page.dart';
 import 'package:restaurian/ui/restaurant_list_page.dart';
 import 'package:restaurian/ui/splash_screen_page.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final ApiService _apiService = ApiService();
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Restaurian',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          scaffoldBackgroundColor: Colors.white,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          textTheme: myTextTheme,
-          appBarTheme: AppBarTheme(
-            titleTextStyle: myTextTheme.headline6,
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          ChangeNotifierProvider<RestaurantsProvider>(
+            create: (_) => RestaurantsProvider(
+              apiService: _apiService,
+            ),
           ),
+          ChangeNotifierProvider<RestaurantDetailProvider>(
+            create: (_) => RestaurantDetailProvider(
+              apiService: _apiService,
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Restaurian',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            scaffoldBackgroundColor: Colors.white,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: myTextTheme,
+            appBarTheme: AppBarTheme(
+              titleTextStyle: myTextTheme.headline6,
+            ),
+          ),
+          initialRoute: SplashScreenPage.routeName,
+          routes: {
+            SplashScreenPage.routeName: (context) => const SplashScreenPage(),
+            RestaurantListPage.routeName: (context) =>
+                const RestaurantListPage(),
+            RestaurantDetailsPage.routeName: (context) => RestaurantDetailsPage(
+                restaurant:
+                    ModalRoute.of(context)?.settings.arguments as Restaurant),
+          },
         ),
-        initialRoute: SplashScreenPage.routeName,
-        routes: {
-          SplashScreenPage.routeName: (context) => const SplashScreenPage(),
-          RestaurantListPage.routeName: (context) => const RestaurantListPage(),
-          RestaurantDetailsPage.routeName: (context) => RestaurantDetailsPage(
-              restaurant:
-                  ModalRoute.of(context)?.settings.arguments as Restaurant),
-        },
       );
 }
