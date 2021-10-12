4c09e71a53ff72e106fba11484f49d693faa2045
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-10-12 09:30:21 +0700

    Add some fields

--- lib/data/model/restaurant.dart
+++ lib/data/model/restaurant.dart
@@ -1,3 +1,7 @@
+import 'package:restaurian/data/model/category.dart';
+import 'package:restaurian/data/model/customer_review.dart';
+import 'package:restaurian/utils/constant.dart';
+
 import 'menu.dart';
 
 class Restaurant {
@@ -6,8 +10,11 @@ class Restaurant {
   final String description;
   final String pictureId;
   final String city;
+  final String address;
   final num rating;
   final Menu menus;
+  final List<Category> categories;
+  final List<CustomerReview> customerReviews;
 
   Restaurant({
     required this.id,
@@ -15,8 +22,11 @@ class Restaurant {
     required this.description,
     required this.pictureId,
     required this.city,
+    required this.address,
     required this.rating,
     required this.menus,
+    required this.categories,
+    required this.customerReviews,
   });
 
   factory Restaurant.fromJson(Map<String, dynamic> json) {
@@ -25,8 +35,22 @@ class Restaurant {
     String description = json['description'];
     String pictureId = json['pictureId'];
     String city = json['city'];
+    String address = json['address'] ?? "";
     num rating = json['rating'];
-    Menu menus = Menu.fromJson(json['menus']);
+    Menu menus = json['menus'] != null
+        ? Menu.fromJson(json['menus'])
+        : Menu(foods: [], drinks: []);
+    List<Category> categories = json['categories'] != null
+        ? (json['categories'] as List)
+            .map((categoryJson) => Category.fromJson(categoryJson))
+            .toList()
+        : [];
+    List<CustomerReview> customerReviews = json['customerReviews'] != null
+        ? (json['customerReviews'] as List)
+            .map((customerReviewJson) =>
+                CustomerReview.fromJson(customerReviewJson))
+            .toList()
+        : [];
 
     return Restaurant(
       id: id,
@@ -34,8 +58,17 @@ class Restaurant {
       description: description,
       pictureId: pictureId,
       city: city,
+      address: address,
       rating: rating,
       menus: menus,
+      categories: categories,
+      customerReviews: customerReviews,
     );
   }
+
+  get smallPictureUrl => '$baseUrl/images/small/$pictureId';
+
+  get mediumPictureUrl => '$baseUrl/images/medium/$pictureId';
+
+  get largePictureUrl => '$baseUrl/images/large/$pictureId';
 }
