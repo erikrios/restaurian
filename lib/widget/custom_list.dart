a712ecfb0d6cca1433ceb8e3466b322834c85c67
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-10-12 12:50:01 +0700

    Create custom_list.dart

---
+++ lib/widget/custom_list.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurian/common/styles.dart';
+import 'package:restaurian/data/model/restaurant.dart';
+import 'package:restaurian/provider/restaurant_detail_provider.dart';
+import 'package:restaurian/ui/restaurant_details_page.dart';
+
+class CustomList extends StatelessWidget {
+  final List<Restaurant> restaurants;
+
+  const CustomList({
+    Key? key,
+    required this.restaurants,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: restaurants.length,
+      separatorBuilder: (context, index) {
+        return const Divider(
+          height: 2.0,
+          color: Colors.grey,
+        );
+      },
+      itemBuilder: (context, index) {
+        Restaurant restaurant = restaurants[index];
+        return _buildItemList(context, restaurant);
+      },
+    );
+  }
+
+  Widget _buildItemList(BuildContext context, Restaurant restaurant) {
+    return ListTile(
+      leading: Hero(
+        tag: restaurant.id,
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+            restaurant.smallPictureUrl,
+          ),
+        ),
+      ),
+      title: Text(
+        restaurant.name,
+        overflow: TextOverflow.ellipsis,
+      ),
+      onTap: () {
+        RestaurantDetailProvider provider =
+            Provider.of<RestaurantDetailProvider>(
+          context,
+          listen: false,
+        );
+        provider.getRestaurant(restaurant.id);
+
+        Navigator.pushNamed(
+          context,
+          RestaurantDetailsPage.routeName,
+        );
+      },
+      subtitle: Row(
+        children: [
+          Flexible(
+            flex: 1,
+            child: SizedBox(
+              width: double.infinity,
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    const WidgetSpan(
+                      child: Icon(
+                        Icons.location_on,
+                        size: 15.0,
+                      ),
+                    ),
+                    TextSpan(
+                      text: restaurant.city,
+                      style: myTextTheme.subtitle2!.copyWith(
+                        color: Colors.black,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RichText(
+            text: TextSpan(
+              children: [
+                WidgetSpan(
+                  child: Icon(
+                    restaurant.rating < 2.0
+                        ? Icons.star_outline
+                        : restaurant.rating < 4.0
+                            ? Icons.star_half
+                            : Icons.star,
+                    size: 15.0,
+                  ),
+                ),
+                TextSpan(
+                  text: restaurant.rating.toString(),
+                  style: myTextTheme.subtitle2!.copyWith(
+                    color: Colors.black,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
