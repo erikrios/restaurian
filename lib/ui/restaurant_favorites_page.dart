f2514f192c2be6c3adc109b627fe3f6196babfe6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-10-16 09:35:32 +0700

    Create restaurant_favorites_page.dart

---
+++ lib/ui/restaurant_favorites_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:restaurian/widget/custom_list.dart';
+
+class RestaurantFavorite extends StatelessWidget {
+  const RestaurantFavorite({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorites'),
+      ),
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: CustomList(
+            restaurants: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
